6bab6f2a38403c0a75e9fe8339f7329d5e618a20
gustavo <gustavolzss12@gmail.com>
2022-06-23 21:43:45 -0300

    initial commit

---
+++ src/test/java/br/com/guinarangers/guinaApi/GuinaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guinarangers.guinaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuinaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
