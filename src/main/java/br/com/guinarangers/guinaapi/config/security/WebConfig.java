2034ba67d770e048db6d2354ead84f5bc89e1457
gustavisk <gustavolzss12@gmail.com>
2022-09-09 00:59:04 -0300

    Nova versão do Docker: 0.0.3

--- src/main/java/br/com/guinarangers/guinaapi/config/security/WebConfig.java
+++ src/main/java/br/com/guinarangers/guinaapi/config/security/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000/")
+            .allowedOrigins("http://127.0.0.1:3000/")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true).maxAge(3600);
