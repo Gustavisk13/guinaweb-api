ea13caf1cc29fdb9e1d08ad85509bdbb936f7cde
gustavisk <gustavolzss12@gmail.com>
2022-09-08 22:47:14 -0300

    Nova versão do Docker: 0.0.2

--- build.sh
+++ build.sh
@@ -3,14 +3,22 @@
 USERNAME=gustavisk
 IMAGE=guina-api
 
+read -p 'Insira a versão do git: ' VERSIONGIT
+
+git pull
+git add .
+git commit -a -m"Nova versão do Docker: $VERSIONGIT"
+git tag $VERSIONGIT HEAD
+git push
+
 mvn clean package -Dmaven.test.skip
 
 docker build -t $USERNAME/$IMAGE:latest .
 
-read -p 'Insira a versão da imagem: ' VERSION
 
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
+
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSIONGIT
 
 docker push $USERNAME/$IMAGE:latest
 
-docker push $USERNAME/$IMAGE:$VERSION
+docker push $USERNAME/$IMAGE:$VERSIONGIT
--- src/main/java/br/com/guinarangers/guinaapi/controller/TesteController.java
+++ src/main/java/br/com/guinarangers/guinaapi/controller/TesteController.java
@@ -1,14 +1,20 @@
 package br.com.guinarangers.guinaapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/test")
+
 public class TesteController {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
     
-    @RequestMapping
-    public String test() {
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("profile", activeProfile.toUpperCase());
         return "teste";
     }
 
