712f311755d971cdca9da1e858057a278ae49f8a
gustavisk <gustavolzss12@gmail.com>
2022-10-30 19:20:22 -0300

    iniciado estudos de conexao com vault

--- src/main/java/br/com/guinarangers/guinaapi/GuinaApiApplication.java
+++ src/main/java/br/com/guinarangers/guinaapi/GuinaApiApplication.java
@@ -1,12 +1,17 @@
 package br.com.guinarangers.guinaapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import br.com.guinarangers.guinaapi.config.security.VaultConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -16,8 +21,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class GuinaApiApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(GuinaApiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(GuinaApiApplication.class, args);
+
+		ConfigurableApplicationContext context =  SpringApplication.run(GuinaApiApplication.class, args);
+
+		VaultConfiguration vault =  context.getBean(VaultConfiguration.class);
+
+		logger.info("Login: " + vault.getLogin());
+		logger.info("Password: " + vault.getPassword());
 	}
 
 }
---
+++ src/main/java/br/com/guinarangers/guinaapi/config/security/VaultConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.guinarangers.guinaapi.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VaultConfiguration {
+
+    @Value("${login}")
+    private String login;
+
+    @Value("${password}")
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    
+    
+}
